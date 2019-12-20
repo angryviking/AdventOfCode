28e4114c1cf7295ed7d71ceac1dc03d1e68e4202
angryviking <mhanna72@gmail.com>
2019-12-20 05:59:18 -0500

    Update

--- 2019/day6/day6-2.sh
+++ 2019/day6/day6-2.sh
@@ -1,35 +1,20 @@
 #!/bin/bash
 
-unset orbiting
-sum=0
-start="YOU"
-end="SAN"
-
 find_path(){
-for i in $(grep "${orbiting}" day6.txt | grep -v "${start}"); do
-  if [ $(echo ${i} | grep "^${orbiting}") ]; then
-    options+=(${i#*)})
-  else 
-    options+=(${i%)*})
-  fi
-done
+  start=$(grep "${person}" day6.txt)
+  path+=(${start%)*})
+  while [ $(grep ")${start%)*}" day6.txt) ]; do
+    new_start=$(grep ")${start%)*}" day6.txt)
+    path+=(${new_start%)*})
+    start=(${new_start%)*})
+  done
+  echo ${path[@]}
 }
 
-orbiting=$(grep "${start}" day6.txt | awk -F')' '{print $1}')
+unset path
+person="YOU"
 find_path
-for p in ${options[@]}; do
-  orbiting=${p}
-  find_path
-done
-echo ${options[@]}
 
-unset options
-
-start="SAN"
-orbiting=$(grep "${start}" day6.txt | awk -F')' '{print $1}')
+unset path
+person="SAN"
 find_path
-for p in ${options2[@]}; do
-  orbiting=${p}
-  find_path
-done
-echo ${options[@]}
