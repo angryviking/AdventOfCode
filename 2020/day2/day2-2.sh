222a5e3b05f706011bf43b94850072c2fe45cc0c
angryviking <mhanna72@gmail.com>
2020-12-02 05:46:52 -0500

    updated day2

--- 2020/day2/day2-2.sh
+++ 2020/day2/day2-2.sh
@@ -9,12 +9,10 @@ done < day2.txt
 sum=0
 count=0
 while [ ${count} -lt ${#key[@]} ]; do
-  pos1=$(cut -d"-" -f1 <<< ${repeat[${count}]})
-  ((pos1--))
-  charpos1=${pass[${count}]:${pos1#*${pass[${count}]}}:1}
-  pos2=$(cut -d"-" -f2 <<< ${repeat[${count}]})
-  ((pos2--))
-  charpos2=${pass[${count}]:${pos2#*${pass[${count}]}}:1}
+  pos1=$(($(cut -d"-" -f1 <<< ${repeat[${count}]})-1))
+  charpos1=${pass[${count}]:${pos1}:1}
+  pos2=$(($(cut -d"-" -f2 <<< ${repeat[${count}]})-1))
+  charpos2=${pass[${count}]:${pos2}:1}
   if [[ ${charpos1} == ${key[${count}]:0:1} || ${charpos2} == ${key[${count}]:0:1} ]]; then
     ((sum++))
     if [[ ${charpos1} == ${key[${count}]:0:1} && ${charpos2} == ${key[${count}]:0:1} ]]; then
