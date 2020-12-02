fd5918360aff5ee8f0ef355b29d6513eab92d84b
angryviking <mhanna72@gmail.com>
2020-12-01 21:13:02 -0500

    updates

--- 2020/day1/day1-2.sh
+++ 2020/day1/day1-2.sh
@@ -3,11 +3,11 @@
 num1+=($(cat day1.txt))
 
 count=0
-while [ ${count} -lt ${#num1[*]} ]; do
+while [ ${count} -lt ${#num1[@]} ]; do
   count2=$((${count}+1))
-  while [ ${count2} -lt ${#num1[*]} ]; do
+  while [ ${count2} -lt ${#num1[@]} ]; do
     count3=$((${count2}+1))
-    while [ ${count3} -lt ${#num1[*]} ]; do
+    while [ ${count3} -lt ${#num1[@]} ]; do
       if [[ $((${num1[${count}]}+${num1[${count2}]}+${num1[${count3}]})) -eq 2020 ]]; then
         echo $((${num1[${count}]}*${num1[${count2}]}*${num1[${count3}]}))
         exit
