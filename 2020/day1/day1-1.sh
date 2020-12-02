aacc5393ebb7bc291f8c69a3372cfbfeb28ff19d
angryviking <mhanna72@gmail.com>
2020-12-01 21:08:36 -0500

    updates

--- 2020/day1/day1-1.sh
+++ 2020/day1/day1-1.sh
@@ -2,10 +2,8 @@
 
 for i in $(cat day1.txt); do
   num2=$(grep -Fx $((2020-${i})) day1.txt)
-  
   if [[ ! -z ${num2} ]]; then
-    product=$((${i}*${num2}))
-    echo ${product}
+    echo $((${i}*${num2}))
     exit
   fi
 done
--- 2020/day1/day1-2.sh
+++ 2020/day1/day1-2.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-for i in $(cat day1.txt); do
-  num1+=(${i})
-done
+num1+=($(cat day1.txt))
 
 count=0
 while [ ${count} -lt ${#num1[*]} ]; do
   count2=$((${count}+1))
   while [ ${count2} -lt ${#num1[*]} ]; do
-    count3=$((count2+1))
+    count3=$((${count2}+1))
     while [ ${count3} -lt ${#num1[*]} ]; do
       if [[ $((${num1[${count}]}+${num1[${count2}]}+${num1[${count3}]})) -eq 2020 ]]; then
-        product=$((${num1[${count}]}*${num1[${count2}]}*${num1[${count3}]}))
-        echo $product
+        echo $((${num1[${count}]}*${num1[${count2}]}*${num1[${count3}]}))
         exit
       else
         count3=$((${count3}+1))
