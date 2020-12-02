0bf49860df14f89df57aabea3e26d4d1da207cf6
angryviking <mhanna72@gmail.com>
2020-12-02 05:38:54 -0500

    updated day2

--- 2020/day2/day2-2.sh
+++ 2020/day2/day2-2.sh
@@ -15,7 +15,6 @@ while [ ${count} -lt ${#key[@]} ]; do
   pos2=$(cut -d"-" -f2 <<< ${repeat[${count}]})
   ((pos2--))
   charpos2=${pass[${count}]:${pos2#*${pass[${count}]}}:1}
-#  num=$(awk -F"${key[${count}]:0:1}" '{ print NF-1 }' <<< ${pass[${count}]})
   if [[ ${charpos1} == ${key[${count}]:0:1} || ${charpos2} == ${key[${count}]:0:1} ]]; then
     ((sum++))
     if [[ ${charpos1} == ${key[${count}]:0:1} && ${charpos2} == ${key[${count}]:0:1} ]]; then
