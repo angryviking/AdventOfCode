17f5b5ea6717f0c794099ff8c1df221e59ee8820
angryviking <mhanna72@gmail.com>
2020-12-03 13:01:15 -0500

    update day3

--- 2020/day3/day3-2.sh
+++ 2020/day3/day3-2.sh
@@ -3,8 +3,8 @@
 find_trees() {
   trees=0
   count=0
-  for i in $(tail +${startline} day3.txt); do
-    if [[ ${startline} -eq 2 || $((${startline}%2)) -eq 1 ]]; then
+  for i in $(tail +${start} day3.txt); do
+    if [[ ${start} -eq 2 || $((${start}%2)) -eq 1 ]]; then
       ((count+=${slope}))
       if [[ ${count} -gt 30 ]]; then
         ((count-=31))
@@ -13,22 +13,20 @@ find_trees() {
         ((trees++))
       fi
     fi
-    if [[ ${skiplines} -eq 1 ]]; then
-      ((startline++))
+    if [[ ${start} -ne 2 ]]; then
+      ((start++))
     fi
   done
 }
 
 product=1
 for slope in 1 3 5 7; do
-  startline=2
-  skiplines=0
+  start=2
   find_trees
   product=$((${trees}*${product}))
 done
 
-skiplines=1
-startline=3
+start=3
 slope=1
 find_trees
 product=$((${trees}*${product}))
