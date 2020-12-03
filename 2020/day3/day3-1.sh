3ae5f69d2819d387fe2d49a30c5caaccdf3622b6
angryviking <mhanna72@gmail.com>
2020-12-03 12:43:22 -0500

    updated day3

--- 2020/day3/day3-1.sh
+++ 2020/day3/day3-1.sh
@@ -4,20 +4,9 @@ trees=0
 count=0
 for i in $(tail +2 day3.txt); do
   ((count+=3))
-  case ${count} in
-    31)
-        count=0
-        ;;
-    32)
-        count=1
-        ;;
-    33)
-        count=2
-        ;;
-    *)
-        ;;
-    esac
-  
+  if [[ ${count} -gt 30 ]]; then
+    count=$((${count}-31))
+  fi
   if [[ ${i:${count}:1} == "#" ]]; then
     ((trees++))
   fi
