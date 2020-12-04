b2f8df65aacf1fdeca366362ed600adcf209ccb4
angryviking <mhanna72@gmail.com>
2020-12-04 07:01:36 -0500

    updated day4

--- 2020/day4/day4-2.sh
+++ 2020/day4/day4-2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+valid=0
 while read line; do
   if [[ ${line} =~ (byr:19[2-9][0-9]|byr:200[0-2]) && \
         ${line} =~ (iyr:201[0-9]|iyr:2020) && \
@@ -8,8 +9,8 @@ while read line; do
         ${line} =~ (hcl:'#'[0-9a-f]{6}) && \
         ${line} =~ (ecl:amb|ecl:blu|ecl:brn|ecl:grn|ecl:gry|ecl:hzl|ecl:oth) && \
         ${line} =~ (pid:[0-9]{9}) ]]; then
-    passports+=("${line}")
+    ((valid++))
   fi
 done < <(awk -v RS= '$1=$1' day4.txt | awk '/byr:/ && /iyr:/ && /eyr:/ && /hgt:/ && /hcl:/ && /ecl:/ && /pid:/')
 
-echo ${#passports[@]}
+echo ${valid}
