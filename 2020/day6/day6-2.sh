a429c5ff40f4f0076843c14dc35c65dfbfcd06f7
angryviking <mhanna72@gmail.com>
2020-12-06 07:42:05 -0500

    update day6

--- 2020/day6/day6-2.sh
+++ 2020/day6/day6-2.sh
@@ -11,17 +11,15 @@ while read line; do
 done < day6.txt
 
 count=0
-total=0
+sum=0
 while read line; do
-  sum=0
   for i in $(echo ${line} | fold -w1 | sort -u); do
     num="${line//[^${i}]}"
     if [[ ${#num} -eq ${ans[${count}]} ]]; then
-      sum=$((${#num}+${sum}))
-      ((total++))
+      ((sum++))
     fi
   done
   ((count++))
 done < <(awk -v RS= '$1=$1' day6.txt)
 
-echo ${total}
+echo ${sum}
