73eb0a22f9b59883d0ac26f47160fb43e47cac2a
angryviking <mhanna72@gmail.com>
2020-12-09 19:18:29 -0500

    update day8

---
+++ 2020/day8/day8-2.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+input=day8.txt
+
+reset_array(){
+  unset oper
+  unset arg
+  count=0
+  for i in $(cat ${input}); do
+    if [[ $((${count}%2)) -eq 0 ]]; then
+      oper+=(${i})
+    else
+      arg+=(${i})
+    fi
+    ((count++))
+  done
+}
+
+get_acc() {
+  bad=0
+  accumulator=0
+  count=0
+  while [ ${count} -lt ${#oper[@]} ]; do
+    case ${oper[${count}]} in
+      nop)
+           if [[ " ${nops[@]} " =~ " ${count} " ]]; then
+             bad=1
+             break
+           fi
+           nops+=(${count})
+           ((count++))
+           if [[ ${count} -eq ${#oper[@]} ]]; then
+             echo ${accumulator}
+             exit
+           fi
+           ;;
+      acc)
+           if [[ " ${accs[@]} " =~ " ${count} " ]]; then
+             bad=1
+             break
+           fi
+           accs+=(${count})
+           dir=${arg[${count}]::1}
+           num=${arg[${count}]:1}
+           ((accumulator$dir=${num}))
+           ((count++))
+           if [[ ${count} -eq ${#oper[@]} ]]; then
+             echo ${accumulator}
+             exit
+           fi
+           ;;
+      jmp)
+           if [[ " ${jmps[@]} " =~ " ${count} " ]]; then
+             bad=1
+             break
+           fi
+           jmps+=(${count})
+           dir=${arg[${count}]::1}
+           num=${arg[${count}]:1}
+           ((count$dir=${num}))
+           if [[ ${count} -eq ${#oper[@]} ]]; then
+             echo ${accumulator}
+             exit
+           fi
+           ;;
+      *)
+           echo "Something is not right here."
+           exit
+           ;;
+    esac
+  done
+}
+
+reset_array
+for ((n=0; n<${#oper[@]}; n++)); do
+  if [[ ${oper[${n}]} == "nop" ]]; then
+    oper[${n}]=jmp
+    get_acc
+    if [[ ${bad} -eq 1 ]]; then
+      reset_array
+    fi
+  elif [[ ${oper[${n}]} == "jmp" ]]; then
+    oper[${n}]=nop
+    get_acc
+    if [[ ${bad} -eq 1 ]]; then
+      reset_array
+    fi
+  fi
+  unset nops
+  unset accs
+  unset jmps
+done
