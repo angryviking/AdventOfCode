4209c0c96a7a4a6ffe560bb4c2d7cb5b952bb1b1
angryviking <mhanna72@gmail.com>
2020-12-11 16:00:47 -0500

    update day7

--- 2020/day7/day7-2.sh
+++ 2020/day7/day7-2.sh
@@ -2,13 +2,11 @@
 
 input=day7.txt
 output=day7_out.txt
-bag="shiny gold"
-rm -rf ${output}
 
-grep ^"${bag}" ${input} | sed -r 's/^(\S+) (\S+) bags contain /\1_\2=$((/g' \
-                        | sed -r 's/([0-9]+) (\S+) (\S+) bags?,/(\1*(\2 \3)) +/g' \
-                        | sed -r 's/([0-9]+) (\S+) (\S+) bags?./(\1*(\2 \3))))/g' \
-                        | sed 's/no other bags./1))/g' > ${output}
+grep ^"shiny gold" ${input} | sed -r 's/^(\S+) (\S+) bags contain /\1_\2=$((/g' \
+                            | sed -r 's/([0-9]+) (\S+) (\S+) bags?,/(\1*(\2 \3)) +/g' \
+                            | sed -r 's/([0-9]+) (\S+) (\S+) bags?./(\1*(\2 \3))))/g' \
+                            | sed 's/no other bags./1))/g' > ${output}
 
 while [[ $(grep -oE "[a-z]+ [a-z]+" ${output}) ]]; do
   while read line; do
