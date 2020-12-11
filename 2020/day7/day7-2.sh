c31bee1d3796773daa4402d4362bd63cb626b365
angryviking <mhanna72@gmail.com>
2020-12-11 17:15:59 -0500

    update day7

--- 2020/day7/day7-2.sh
+++ 2020/day7/day7-2.sh
@@ -3,21 +3,19 @@
 input=day7.txt
 output=day7_out.txt
 
-grep ^"shiny gold" ${input} | sed -r 's/^(\S+) (\S+) bags contain /\1_\2=$((/g' \
-                            | sed -r 's/([0-9]+) (\S+) (\S+) bags?,/(\1*(\2 \3)) +/g' \
-                            | sed -r 's/([0-9]+) (\S+) (\S+) bags?./(\1*(\2 \3))))/g' \
-                            | sed 's/no other bags./1))/g' > ${output}
+grep ^"shiny gold" ${input} | sed -r 's/^(\S+) (\S+) bags contain/\1_\2=$((/g' \
+                            | sed -r 's/ ([0-9]+) (\S+) (\S+) bags?,/(\1*(\2 \3))+/g' \
+                            | sed -r 's/ ([0-9]+) (\S+) (\S+) bags?./(\1*(\2 \3))))/g' > ${output}
 
 while [[ $(grep -oE "[a-z]+ [a-z]+" ${output}) ]]; do
   while read line; do
-    replace=$(grep ^"${line}" ${input} | sed -r 's/^(\S+) (\S+) bags contain //g' \
-                                       | sed -r 's/([0-9]+) (\S+) (\S+) bags?,/(\1*(\2 \3)) +/g' \
-                                       | sed -r 's/([0-9]+) (\S+) (\S+) bags?./(\1*(\2 \3)) + 1/g' \
-                                       | sed 's/no other bags./1/g')
+    replace=$(grep ^"${line}" ${input} | sed -r 's/^(\S+) (\S+) bags contain//g' \
+                                       | sed -r 's/ ([0-9]+) (\S+) (\S+) bags?,/(\1*(\2 \3))+/g' \
+                                       | sed -r 's/ ([0-9]+) (\S+) (\S+) bags?./(\1*(\2 \3))+1/g' \
+                                       | sed 's/ no other bags./1/g')
     sed -i 's/'"${line}"'/'"${replace}"'/' ${output}
   done < <(grep -oE "[a-z]+ [a-z]+" ${output})
 done
 
-source ${output}
-echo $((${shiny_gold}))
+source ${output} && echo $((${shiny_gold}))
 rm -rf ${output}
