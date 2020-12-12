56089b208686d291a3f89e5de0fcc6b6ba9bcafe
angryviking <mhanna72@gmail.com>
2020-12-12 07:17:54 -0500

    update day12

--- 2020/day12/day12-1.sh
+++ 2020/day12/day12-1.sh
@@ -11,99 +11,99 @@ facing=east
 
 while read line; do
   case ${line:0:1} in
-      F)
-          if [[ ${facing} == "east" ]]; then
-            ((y+=${line:1:${#line}}))
-          elif [[ ${facing} == "west" ]]; then
-            ((y-=${line:1:${#line}}))
-          elif [[ ${facing} == "north" ]]; then
-            ((x+=${line:1:${#line}}))
-          elif [[ ${facing} == "south" ]]; then
-            ((x-=${line:1:${#line}}))
-          fi
-          ;;
-      L)
-          if [[ ${line:1:${#line}} -eq 90 ]]; then
-            if [[ ${facing} == "east" ]]; then
-              facing=north
-            elif [[ ${facing} == "north" ]]; then
-              facing=west
-            elif [[ ${facing} == "west" ]]; then
-              facing=south
-            elif [[ ${facing} == "south" ]]; then
-              facing=east
-            fi
-          elif [[ ${line:1:${#line}} -eq 180 ]]; then
-            if [[ ${facing} == "east" ]]; then
-              facing=west
-            elif [[ ${facing} == "north" ]]; then
-              facing=south
-            elif [[ ${facing} == "west" ]]; then
-              facing=east
-            elif [[ ${facing} == "south" ]]; then
-              facing=north
-            fi
-          elif [[ ${line:1:${#line}} -eq 270 ]]; then
-            if [[ ${facing} == "east" ]]; then
-              facing=south
-            elif [[ ${facing} == "north" ]]; then
-              facing=east
-            elif [[ ${facing} == "west" ]]; then
-              facing=north
-            elif [[ ${facing} == "south" ]]; then
-              facing=west
-            fi
-          fi
-          ;;
-      R)
-           if [[ ${line:1:${#line}} -eq 90 ]]; then
-            if [[ ${facing} == "east" ]]; then
-              facing=south
-            elif [[ ${facing} == "north" ]]; then
-              facing=east
-            elif [[ ${facing} == "west" ]]; then
-              facing=north
-            elif [[ ${facing} == "south" ]]; then
-              facing=west
-            fi
-          elif [[ ${line:1:${#line}} -eq 180 ]]; then
-            if [[ ${facing} == "east" ]]; then
-              facing=west
-            elif [[ ${facing} == "north" ]]; then
-              facing=south
-            elif [[ ${facing} == "west" ]]; then
-              facing=east
-            elif [[ ${facing} == "south" ]]; then
-              facing=north
-            fi
-          elif [[ ${line:1:${#line}} -eq 270 ]]; then
-            if [[ ${facing} == "east" ]]; then
-              facing=noth
-            elif [[ ${facing} == "north" ]]; then
-              facing=west
-            elif [[ ${facing} == "west" ]]; then
-              facing=south
-            elif [[ ${facing} == "south" ]]; then
-              facing=east
-            fi
-          fi
-          ;;
-      N)
-          ((x+=${line:1:${#line}}))
-          ;;
-      S)
-          ((x-=${line:1:${#line}}))
-          ;;
-      E)
+    F)
+        if [[ ${facing} == "east" ]]; then
           ((y+=${line:1:${#line}}))
-          ;;
-      W)
+        elif [[ ${facing} == "west" ]]; then
           ((y-=${line:1:${#line}}))
-          ;;
-      *)
-          echo "Something's not right here."
-          exit
-          ;;
+        elif [[ ${facing} == "north" ]]; then
+          ((x+=${line:1:${#line}}))
+        elif [[ ${facing} == "south" ]]; then
+          ((x-=${line:1:${#line}}))
+        fi
+        ;;
+    L)
+        if [[ ${line:1:${#line}} -eq 90 ]]; then
+          if [[ ${facing} == "east" ]]; then
+            facing=north
+          elif [[ ${facing} == "north" ]]; then
+            facing=west
+          elif [[ ${facing} == "west" ]]; then
+            facing=south
+          elif [[ ${facing} == "south" ]]; then
+            facing=east
+          fi
+        elif [[ ${line:1:${#line}} -eq 180 ]]; then
+          if [[ ${facing} == "east" ]]; then
+            facing=west
+          elif [[ ${facing} == "north" ]]; then
+            facing=south
+          elif [[ ${facing} == "west" ]]; then
+            facing=east
+          elif [[ ${facing} == "south" ]]; then
+            facing=north
+          fi
+        elif [[ ${line:1:${#line}} -eq 270 ]]; then
+          if [[ ${facing} == "east" ]]; then
+            facing=south
+          elif [[ ${facing} == "north" ]]; then
+            facing=east
+          elif [[ ${facing} == "west" ]]; then
+            facing=north
+          elif [[ ${facing} == "south" ]]; then
+            facing=west
+          fi
+        fi
+        ;;
+    R)
+        if [[ ${line:1:${#line}} -eq 90 ]]; then
+          if [[ ${facing} == "east" ]]; then
+            facing=south
+          elif [[ ${facing} == "north" ]]; then
+            facing=east
+          elif [[ ${facing} == "west" ]]; then
+            facing=north
+          elif [[ ${facing} == "south" ]]; then
+            facing=west
+          fi
+        elif [[ ${line:1:${#line}} -eq 180 ]]; then
+          if [[ ${facing} == "east" ]]; then
+            facing=west
+          elif [[ ${facing} == "north" ]]; then
+            facing=south
+          elif [[ ${facing} == "west" ]]; then
+            facing=east
+          elif [[ ${facing} == "south" ]]; then
+            facing=north
+          fi
+        elif [[ ${line:1:${#line}} -eq 270 ]]; then
+          if [[ ${facing} == "east" ]]; then
+            facing=noth
+          elif [[ ${facing} == "north" ]]; then
+            facing=west
+          elif [[ ${facing} == "west" ]]; then
+            facing=south
+          elif [[ ${facing} == "south" ]]; then
+            facing=east
+          fi
+        fi
+        ;;
+    N)
+        ((x+=${line:1:${#line}}))
+        ;;
+    S)
+        ((x-=${line:1:${#line}}))
+        ;;
+    E)
+        ((y+=${line:1:${#line}}))
+        ;;
+    W)
+        ((y-=${line:1:${#line}}))
+        ;;
+    *)
+        echo "Something's not right here."
+        exit
+        ;;
   esac
 done < ${input}
 
