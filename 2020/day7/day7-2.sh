7bcfffb90dafbf18312c0a48b0d6abc145fb2649
angryviking <mhanna72@gmail.com>
2020-12-12 15:59:50 -0500

    update day7

--- 2020/day7/day7-2.sh
+++ 2020/day7/day7-2.sh
@@ -14,7 +14,7 @@ while [[ $(grep -oE "[a-z]+ [a-z]+" <<< ${expression}) ]]; do
       sed -r 's/ ([0-9]+) (\S+) (\S+) bags?,/(\1*(\2 \3))+/g' | \
       sed -r 's/ ([0-9]+) (\S+) (\S+) bags?./(\1*(\2 \3))+1/g' | \
       sed 's/ no other bags./1/g')
-    expression=$(sed 's/'"${line}"'/'"${replace}"'/' <<< ${expression})
+    expression=$(sed "s/${line}/${replace}/" <<< ${expression})
   done < <(grep -oE "[a-z]+ [a-z]+" <<< ${expression})
 done
 
