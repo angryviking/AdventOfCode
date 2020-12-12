1bd4f7d37334569c2b1f1f49435949b597324641
angryviking <mhanna72@gmail.com>
2020-12-12 15:50:26 -0500

    update day7

--- 2020/day7/day7-2.sh
+++ 2020/day7/day7-2.sh
@@ -7,15 +7,15 @@ expression=$(grep ^"shiny gold" ${input} | \
   sed -r 's/ ([0-9]+) (\S+) (\S+) bags?,/(\1*(\2 \3))+/g' | \
   sed -r 's/ ([0-9]+) (\S+) (\S+) bags?./(\1*(\2 \3))/g')
 
-while [[ $(echo ${expression} | grep -oE "[a-z]+ [a-z]+") ]]; do
+while [[ $(grep -oE "[a-z]+ [a-z]+" <<< ${expression}) ]]; do
   while read line; do
     replace=$(grep ^"${line}" ${input} | \
       sed -r 's/^(\S+) (\S+) bags contain//g' | \
       sed -r 's/ ([0-9]+) (\S+) (\S+) bags?,/(\1*(\2 \3))+/g' | \
       sed -r 's/ ([0-9]+) (\S+) (\S+) bags?./(\1*(\2 \3))+1/g' | \
       sed 's/ no other bags./1/g')
-    expression=$(echo ${expression} | sed 's/'"${line}"'/'"${replace}"'/')
-  done < <(echo ${expression} | grep -oE "[a-z]+ [a-z]+")
+    expression=$(sed 's/'"${line}"'/'"${replace}"'/' <<< ${expression})
+  done < <(grep -oE "[a-z]+ [a-z]+" <<< ${expression})
 done
 
 echo $((${expression}))
