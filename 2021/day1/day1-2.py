cdc1165743591fa81563787a27c304d7fe35cd01
angryviking <mhanna72@gmail.com>
2021-12-01 14:32:08 -0500

    Updates

--- 2021/day1/day1-2.py
+++ 2021/day1/day1-2.py
@@ -6,6 +6,6 @@ depths = np.loadtxt("day1.txt", dtype=int)
 for i in range(0, len(depths) - 3):
     if ((depths[i+1] + depths[i+2] + depths[i+3]) > 
         (depths[i] + depths[i+1] + depths[i+2])): 
-       increased += 1
+        increased += 1
 
 print (increased)
