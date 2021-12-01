9d63c2b6ad5d0cada2bf3632c740553e816c6426
angryviking <mhanna72@gmail.com>
2021-12-01 11:09:11 -0500

    Updates

--- 2021/day1/day1-2.py
+++ 2021/day1/day1-2.py
@@ -3,7 +3,7 @@ import numpy as np
 increased = 0
 depths = np.loadtxt("day1.txt", dtype=int)
 
-for i in range(0, len(depths) - 3, 1):
+for i in range(0, len(depths) - 3):
     if ((depths[i+1] + depths[i+2] + depths[i+3]) > 
         (depths[i] + depths[i+1] + depths[i+2])): 
        increased += 1
